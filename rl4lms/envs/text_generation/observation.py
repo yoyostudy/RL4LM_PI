f2b9b60f369353ada23c563eb7647a54576f4d59
yoyo <wangzheyoyo@gmail.com>
2024-04-22 13:13:07 -0700

    add policy_chosen to observation for two-head action | add view_obs.ipynb

--- rl4lms/envs/text_generation/observation.py
+++ rl4lms/envs/text_generation/observation.py
@@ -6,7 +6,6 @@ from transformers import AutoTokenizer
 from rl4lms.data_pools.text_generation_pool import Sample
 from copy import deepcopy
 
-
 @dataclass
 class Observation:
     # encoded input
@@ -31,6 +30,9 @@ class Observation:
     # list of actions
     action_history: List[str]
 
+    # TODO: add policy_chosen
+    policy_chosen: int # -1 for not chosen yet
+
     # other meta info
     meta_info: Dict[str, Any]
 
@@ -44,39 +46,65 @@ class Observation:
             "context_encoded_pt": self.context_encoded_pt.numpy().flatten(),
             "context_attention_mask_pt": self.context_attention_mask_pt.numpy().flatten(),
             "input_encoded_pt": self.input_encoded_pt.numpy().flatten(),
-            "input_attention_mask_pt": self.input_attention_mask_pt.numpy().flatten()
-
+            "input_attention_mask_pt": self.input_attention_mask_pt.numpy().flatten(),
+            # TODO: should I add policy_chosen here?
         }
         return dict_obs
 
     @staticmethod
     def _concat(prompt: torch.tensor, prompt_mask: torch.tensor,
                 context: torch.tensor, context_mask: torch.tensor,
-                pad_token: int):
+                pad_token: int, policy_chosen: int = -1):
+        
+        # TODO: add policy_chosen
+        policy_chosen = torch.tensor([policy_chosen]).expand(1, -1)
 
         prompt_ = prompt[:, prompt_mask.flatten().bool().tolist()]
         context_ = context[:, context_mask.flatten().bool().tolist()]
-        actual_size = prompt_.shape[1] + context_.shape[1]
 
-        full_size = prompt.shape[1] + context.shape[1]
+        # TODO: add policy_chosen to actual size
+        #actual_size = prompt_.shape[1] + context_.shape[1]
+        actual_size = prompt_.shape[1] + context_.shape[1] + policy_chosen.shape[1]
+
+        # TODO: add policy_chosen to full size
+        #full_size = prompt.shape[1] + context.shape[1]
+        full_size = prompt.shape[1] + context.shape[1] + policy_chosen.shape[1]
+
         concatenated = torch.full(
             (full_size,), fill_value=pad_token).reshape(1, -1)
         concatenated_mask = torch.zeros((1, full_size)).int()
 
+        # TODO: add policy_chosen
+        # concatenated[:, full_size -
+        #              actual_size:] = torch.cat((prompt_, context_), dim=1)
         concatenated[:, full_size -
-                     actual_size:] = torch.cat((prompt_, context_), dim=1)
+                        actual_size:] = torch.cat((prompt_, context_, policy_chosen), dim=1)
         concatenated_mask[:, full_size -
                           actual_size:] = 1
         return concatenated, concatenated_mask
 
-    def update(self, action: int, tokenizer: AutoTokenizer) -> "Observation":
+
+    ## TODO: add two head action
+    def update(self, policy_chosen: int, action: int, tokenizer: AutoTokenizer) -> "Observation":
         """
         Updates the observation using the given action
+
+        # TODO: add two head action
+        policy_chosen: int, -1 for not chosen yet, 0 for Seq2Seq, 1 for Casual
+        action: int, the token id of the action
         """
 
         # update the action history
+        # TODO: add two head action, add policy_chosen head
         current_action_history = deepcopy(self.action_history)
-        current_action_history.append(tokenizer._convert_id_to_token(action))
+        policy_token = tokenizer._convert_id_to_token(policy_chosen)
+        action_token = tokenizer._convert_id_to_token(action)
+        current_action_history.append((policy_token, action_token))
+
+        # update the policy_chosen
+        # TODO: make sure the policy only update if it is not chosen yet
+        assert (self.policy_chosen == -1 or self.policy_chosen == policy_chosen), "policy update not correct!"
+        self.policy_chosen = policy_chosen
 
         # get the current context
         current_context = deepcopy(self.context_encoded_pt)
@@ -97,12 +125,14 @@ class Observation:
             current_context.flatten(), skip_special_tokens=True)
 
         # concatenate and still keep the left padding
+        # TODO: add policy_chosen to the obs input
         input_encoded_pt, input_attention_mask_pt = Observation._concat(
             self.prompt_or_input_encoded_pt, self.prompt_or_input_attention_mask_pt,
             current_context, current_context_attention_mask,
-            tokenizer.pad_token_id)
+            tokenizer.pad_token_id, self.policy_chosen)
 
         # and create a new observation
+        # TODO: add policy_chosen
         obs = Observation(self.prompt_or_input_encoded_pt,
                           self.prompt_or_input_attention_mask_pt,
                           self.prompt_or_input_text,
@@ -113,6 +143,7 @@ class Observation:
                           input_encoded_pt,
                           input_attention_mask_pt,
                           current_action_history,
+                          self.policy_chosen,
                           self.meta_info)
 
         return obs
@@ -124,6 +155,7 @@ class Observation:
                          max_context_length: int,
                          prompt_truncation_side: str,
                          context_start_token: int = None,
+                         policy_chosen: int = -1, # -1 for not chosen yet
                          meta_info: Dict[str, Any] = None):
         # encode the prompt text
         # override truncation side for prompt
@@ -172,6 +204,7 @@ class Observation:
                           context_text="",
                           target_or_reference_texts=sample.references,
                           action_history=[],
+                          policy_chosen=-1, # TODO: add policy_chosen for two-head action, initial set to -1 for not chosen yet
                           meta_info=meta_info)
 
         return obs
@@ -181,13 +214,26 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
     tokenizer.pad_token = tokenizer.eos_token
     tokenizer.padding_side = "left"
-    sample = Sample("1", "Hello, this is cool", ["it is good", "going well"])
+    sample = Sample(
+        id = "1", 
+        prompt_or_input_text= "Hello, this is cool", 
+        references= ["it is good", "going well"],
+        meta_data = {"key": "value"})
+
+    print(sample)
 
     obs = Observation.init_from_sample(
         sample=sample,
         tokenizer=tokenizer,
         max_input_length=24,
-        max_context_length=24
+        max_context_length=24,
+        prompt_truncation_side="left", # add this line 
     )
-    updated_obs = obs.update(10, tokenizer)
-    updated_obs = updated_obs.update(11, tokenizer)
+    # TODO: add two head action for update
+    policy_chosen = 0 # action head 1, decision making head
+    action = 10 # action head 2, generation head
+    updated_obs = obs.update(
+        policy_chosen=0, action = 10, tokenizer = tokenizer)
+    
+    print(updated_obs)
+    #updated_obs = updated_obs.update(11, tokenizer)
